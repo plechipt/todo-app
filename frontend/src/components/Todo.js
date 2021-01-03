5c06b1fa2d106767524713ddea67aa0e5136dc9c
Jakub Plecháč <loxtox74@gmail.com>
2021-01-03 11:55:44 +0100

    added AppBar

--- frontend/src/components/Todo.js
+++ frontend/src/components/Todo.js
@@ -1,7 +1,33 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(3),
+    textAlign: "left",
+  },
+}));
 
 const Todo = () => {
-  return null;
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Grid justify="center" alignItems="center" container spacing={3}>
+        <Grid item xs={8}>
+          <Paper className={classes.paper}>xs=12</Paper>
+        </Grid>
+        <Grid item xs={8}>
+          <Paper className={classes.paper}>xs=12</Paper>
+        </Grid>
+      </Grid>
+    </div>
+  );
 };
 
 export default Todo;
