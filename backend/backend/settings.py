2d3f40c82e5dbfb785655d437edcdc0c0296549c
Jakub Plecháč <loxtox74@gmail.com>
2021-01-06 20:37:28 +0100

    added backend

---
+++ backend/backend/settings.py
@@ -0,0 +1,21 @@
+
+import os
+from pathlib import Path
+
+from backend.settings_files.basic import *
+from backend.settings_files.development import *
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = os.environ.get('TODO_APP_SECRET_KEY')
+DEBUG = True
+ALLOWED_HOSTS = []
+
+# Database
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
---
+++ backend/backend/settings_files/development.py
@@ -0,0 +1,15 @@
+
+from pathlib import Path
+
+ROOT_URLCONF = 'backend.urls'
+WSGI_APPLICATION = 'backend.wsgi.application'
+
+# Internationalization
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
