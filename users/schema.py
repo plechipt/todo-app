842b813cd64b07dad5529653312e17ec262d0b15
Jakub Plecháč <loxtox74@gmail.com>
2021-01-08 12:40:47 +0100

    fixed tokenAuth issue

--- users/schema.py
+++ users/schema.py
@@ -1,31 +1,15 @@
 import graphene
-import graphql_jwt
-from django.dispatch import receiver
-from graphql_jwt.refresh_token.signals import refresh_token_rotated
-
 from .mutations.users import *
+from .mutations.jwt import *
 
 
-# Revoke refresh token after it has been used
-@receiver(refresh_token_rotated)
-def revoke_refresh_token(sender, request, refresh_token, **kwargs):
-    refresh_token.revoke(request)
-
-
-class UserMutation:
+class UserMutation(AuthMutation, graphene.ObjectType):
     register = Register.Field()
     login = Login.Field()
     logout = Logout.Field()
     verify_access_token = VerifyAccessToken.Field()
 
-    # Django-graphql-jwt
-    token_auth = graphql_jwt.ObtainJSONWebToken.Field()
-    verify_token = graphql_jwt.Verify.Field()
-    refresh_token = graphql_jwt.Refresh.Field()
-    delete_token_cookie = graphql_jwt.DeleteJSONWebTokenCookie.Field()
-    delete_refresh_token_cookie = graphql_jwt.DeleteRefreshTokenCookie.Field()
-
-
+    
 class UserQuery:
     me = graphene.Field(UserType)
 
