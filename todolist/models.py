42501cd6e2bb0104c0fe941c532d2b95891c27e1
Jakub Plecháč <loxtox74@gmail.com>
2021-01-09 19:16:22 +0100

    defined Todo model

--- todolist/models.py
+++ todolist/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Todo(models.Model):
+    content = models.CharField(max_length=70)
+    completed = models.BooleanField(default=False)
+    date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.content
+
+    class Meta:
+        ordering = ['-date']
+
