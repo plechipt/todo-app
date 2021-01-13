fa1e66301a67915d6074594b9e61c207977f5b69
Jakub Plecháč <loxtox74@gmail.com>
2021-01-13 18:03:09 +0100

    hidden django admin path

--- backend/urls.py
+++ backend/urls.py
@@ -1,3 +1,4 @@
+import os
 from django.contrib import admin
 from django.urls import path, re_path
 from graphene_django.views import GraphQLView
@@ -6,8 +7,10 @@ from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import TemplateView
 from django.middleware.csrf import rotate_token
 
+ADMIN_PATH = os.environ.get('TODO_APP_ADMIN_PATH')
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(f'{ADMIN_PATH}/', admin.site.urls),
     path('graphql/', jwt_cookie(GraphQLView.as_view(graphiql=False))),
     re_path('.*', TemplateView.as_view(template_name='index.html'))
 ]
