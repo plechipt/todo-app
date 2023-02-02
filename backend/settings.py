f5ea942b4439d64153d6eb3c994a81db1dc3eecf
Jakub Plecháč <loxtox74@gmail.com>
2023-02-02 22:40:20 +0100

    added secret_key to settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -9,6 +9,7 @@ from backend.settings_files.graphene import *
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 DEBUG = os.getenv('DEBUG')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # Dotenv
 from dotenv import load_dotenv
