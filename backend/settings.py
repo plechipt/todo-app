681461396832be6f204c24003f17fe17ee93f260
Jakub Plecháč <loxtox74@gmail.com>
2023-02-05 21:48:43 +0100

    returned back default db

--- backend/settings.py
+++ backend/settings.py
@@ -18,12 +18,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Database
 ''''''
 DATABASES = {
-    '''
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-    '''
 }
 
 # Connect postgres 
