3dac1196da67b10d4fa7caa8abec4f5e904a50b2
Jakub Plecháč <plechac.k@gmail.com>
2023-08-20 15:38:50 +0200

    edited build.sh file

--- build.sh
+++ build.sh
@@ -6,6 +6,8 @@ python3.9 -m pip install -r requirements.txt
 
 echo "Make Migration..."
 python3.9 manage.py makemigrations --noinput
+python3.9 manage.py makemigrations users --noinput
+python3.9 manage.py makemigrations todolist --noinput
 python3.9 manage.py migrate --noinput
 
 echo "Collect static..."
