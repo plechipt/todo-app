25caf2fc0f6b5cd42484bf4c15de5e0950be6ca9
Jakub Plecháč <plechac.k@gmail.com>
2023-09-04 15:38:27 +0200

    new server

--- build.sh
+++ build.sh
@@ -5,8 +5,6 @@ echo "Building project"
 python3.9 -m pip install -r requirements.txt
 
 echo "Make Migration..."
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
 python3.9 manage.py makemigrations users --noinput
 python3.9 manage.py makemigrations todolist --noinput
 python3.9 manage.py migrate --noinput
